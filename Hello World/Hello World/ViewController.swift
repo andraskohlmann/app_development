1e2fcc6fcc81cd88935cf83e0517f6c803e55209
Kohlmann András <metuoku@outlook.com>
2018-11-24 20:02:31 +0100

    additional stuff

---
+++ Hello World/Hello World/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Hello World
+//
+//  Created by András Kohlmann on 2018. 11. 19..
+//  Copyright © 2018. András Kohlmann. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
